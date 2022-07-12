b97a820128b37910b36799334a750e4a08892770
JESSE <dtrsjesse121@gmail.com>
2022-07-12 14:50:02 -0400

    Tooled around with datatypes and variable declarations to get a better understanding of how Java accepts and uses data

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -6,6 +6,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //----Primitive Data Types----//
+
+        // int helloWorld = 5;
+        // double numTwo = 5.0;
+        // boolean b = true;
+        // char c = 'f';
+        // int tim = helloWorld;
+
+        //----Not Primitive----//
+
+        // String str = "Hello World!";
+
+        //----Println for all the above variables----//
+        
+        // System.out.println(helloWorld);
+        // System.out.println(numTwo);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(str);
+        // System.out.println(tim);
+        
     }
 }
